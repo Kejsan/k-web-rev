e6c0439ff8f2cda8cc1b6c3664f2b3aedb494ef8
Kejsan Coku <77810310+Kejsan@users.noreply.github.com>
2025-08-22 20:18:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,15 +20,6 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MJFZL8BLMB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MJFZL8BLMB');
-</script>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
